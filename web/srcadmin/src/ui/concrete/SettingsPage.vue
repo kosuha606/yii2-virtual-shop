0ca21a67f351a9f73dc5e1a3e4f706a73469e60f
Kosuha John <kosuha606@yandex.ru>
2020-06-12 23:44:52 +0300

    admin lte integration

--- web/srcadmin/src/ui/concrete/SettingsPage.vue
+++ web/srcadmin/src/ui/concrete/SettingsPage.vue
@@ -1,41 +1,63 @@
 <template>
     <div>
-        <h1>Настройки</h1>
 
-        <hr>
 
-        <ul class="nav nav-tabs">
-            <li :class="'nav-item '+(tab === 'Основные' ? 'active' : '')" v-for="(item, tab) in props.settingsData">
-                <a class="nav-link active" data-toggle="tab" :href="'#'+tab">{{ tab }}</a>
-            </li>
-        </ul>
+        <div class="content-header">
+            <h1>Настройки</h1>
+        </div>
 
-        <div class="tab-content">
-            <p>&nbsp;</p>
 
-            <div :class="'tab-pane fade '+(tab === 'Основные' ? 'active in' : '')"
-                 :id="tab"
-                 v-for="(item, tab) in props.settingsData"
-            >
-                <template v-for="(field, index) in item">
-                    <div class="form-row">
-                        <detail-field
-                                :key="'data_component'+index"
-                                :component="field"
-                        >
-                        </detail-field>
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+
+                        <div class="card">
+                            <div class="card-body">
+
+
+                                <ul class="nav nav-tabs">
+                                    <li :class="'nav-item '+(tab === 'Основные' ? 'active' : '')" v-for="(item, tab) in props.settingsData">
+                                        <a :class="'nav-link'" data-toggle="tab" :href="'#'+tab">{{ tab }}</a>
+                                    </li>
+                                </ul>
+
+                                <div class="tab-content">
+                                    <p>&nbsp;</p>
+
+                                    <div :class="'tab-pane fade '+(tab === 'Основные' ? 'active show' : '')"
+                                         :id="tab"
+                                         v-for="(item, tab) in props.settingsData"
+                                    >
+                                        <template v-for="(field, index) in item">
+                                            <div class="form-row">
+                                                <detail-field
+                                                        :key="'data_component'+index"
+                                                        :component="field"
+                                                >
+                                                </detail-field>
+                                            </div>
+                                        </template>
+                                    </div>
+
+                                </div>
+
+
+
+                                <p>&nbsp;</p>
+                                <div>
+                                    <button @click="onSave" class="btn btn-primary">Сохранить настройки</button>
+                                </div>
+
+
+                            </div>
+                        </div>
+
                     </div>
-                </template>
+                </div>
             </div>
-
         </div>
 
-
-
-        <p>&nbsp;</p>
-        <div>
-            <button @click="onSave" class="btn btn-primary">Сохранить настройки</button>
-        </div>
     </div>
 </template>
 
