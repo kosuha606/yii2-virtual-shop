589b322a5a6c00a36dfc6961698a78b63c9f4a76
Kosuha John <kosuha606@yandex.ru>
2020-06-16 22:02:51 +0300

    seo filter

---
+++ virtualModels/Admin/Domains/Seo/SeoFilterService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\virtualModels\Admin\Domains\Seo;
+
+class SeoFilterService
+{
+    /**
+     * Создать урл из данных фильтров
+     * @param array $data
+     */
+    public function createUrl($data = [])
+    {
+
+    }
+
+    /**
+     * Распарсить урл в данные
+     * @param $url
+     */
+    public function parseUrl($url)
+    {
+
+    }
+}
